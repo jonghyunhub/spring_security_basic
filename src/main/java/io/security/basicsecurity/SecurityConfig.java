9395eac8b8011057421e07433a8c486abaeb7125
chujonghyun <chuwhdgus@gmail.com>
2022-11-23 19:37:32 +0900

    시큐리티 logout 인증 테스트

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -8,10 +8,13 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @Configuration
@@ -24,28 +27,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .anyRequest().authenticated(); // 인가 정책
         http
-                .formLogin() // 인증 정책
-                .loginPage("/loginPage")
-                .defaultSuccessUrl("/") // 인증이 성공 했을때 리다이렉트 되는 유알엘
-                .failureUrl("/login") //인증이 실패 했을때 리다이렉트 되는 유알엘
-                .usernameParameter("userId")
-                .passwordParameter("passwd")
-                .loginProcessingUrl("/login_proc") // 로그인 처리 페이지 uri(사용자 이름과 암호를 제출할 URL)
-                .successHandler(new AuthenticationSuccessHandler() {
+                .formLogin(); // 인증 정책
+        http
+                .logout() // 로그아웃 정책
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login")
+                .addLogoutHandler(new LogoutHandler() {
                     @Override
-                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-                        System.out.println("authentication = " + authentication.getName());
-                        response.sendRedirect("/");
+                    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+                        HttpSession session = request.getSession();
+                        session.invalidate();
                     }
                 })
-                .failureHandler(new AuthenticationFailureHandler() {
+                .logoutSuccessHandler(new LogoutSuccessHandler() {
                     @Override
-                    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-                        System.out.println("exception = " + exception.getMessage());
+                    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
                         response.sendRedirect("/login");
                     }
                 })
-                .permitAll() // loginPage, login, login_proc 은 인증없이 화면을 보여줘야 하므로 이 Url 들을 인증없이 접근 가능하도록 허용
-                ;
+                .deleteCookies("remember-me");
+
     }
 }
